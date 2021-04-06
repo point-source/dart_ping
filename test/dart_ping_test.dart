070ab55399782a61dd427ab2a9f679860dde105b
PointSource <47544779+point-source@users.noreply.github.com>
2021-04-06 16:55:31 -0700

    Fix + add tests

--- test/dart_ping_test.dart
+++ test/dart_ping_test.dart
@@ -5,30 +5,65 @@ import 'package:test/test.dart';
 
 void main() {
   final seq = Platform.operatingSystem == 'macos' ? 0 : 1;
-  group('Testing ping against host: ', () {
+  group('Pinging host: ', () {
     test('google.com', () async {
-      var ping =
-          Ping('google.com', count: 1, timeout: 1, interval: 1, ipv6: false);
+      var ping = Ping('google.com', count: 1);
       var data = await ping.stream.first;
       expect(data, isA<PingData>());
       expect(data.response?.seq, seq);
     });
 
     test('1.1.1.1', () async {
-      var ping =
-          Ping('1.1.1.1', count: 1, timeout: 1, interval: 1, ipv6: false);
+      var ping = Ping('1.1.1.1', count: 1);
       var data = await ping.stream.first;
       expect(data, isA<PingData>());
       expect(data.response?.ip, '1.1.1.1');
       expect(data.response?.seq, seq);
     });
+  });
 
-    test('shouldneverresolve', () async {
-      var ping = Ping('shouldneverresolve',
-          count: 1, timeout: 1, interval: 1, ipv6: false);
+  group('Error handling: ', () {
+    test('Unknown Host', () async {
+      var ping = Ping('shouldneverresolve', count: 1, timeout: 1);
       var data = await ping.stream.first;
       expect(data, isA<PingData>());
       expect(data.error?.error, ErrorType.UnknownHost);
+    });
+
+    test('TTL Exceeded', () async {
+      var ping = Ping('201.202.203.204', count: 2, ttl: 1);
+      var data = await ping.stream.last;
+      expect(data, isA<PingData>());
+      expect(data.error?.error, ErrorType.NoReply);
+    });
+  });
+
+  group('Early termination: ', () {
+    test('google.com', () async {
+      var ping = Ping('google.com', count: 5);
+      var data = <PingData>[];
+      ping.stream.listen(data.add);
+      await Future.delayed(Duration(milliseconds: 1300));
+      await ping.stop();
+      expect(data.first, isA<PingData>());
+      expect(data.last.summary, isNotNull);
+    });
+
+    test('1.1.1.1', () async {
+      var ping = Ping('1.1.1.1', count: 5);
+      var data = <PingData>[];
+      ping.stream.listen(data.add);
+      await Future.delayed(Duration(milliseconds: 1300));
+      await ping.stop();
+      expect(data.first, isA<PingData>());
+      expect(data.last.summary, isNotNull);
+    });
+  });
+
+  group('Misuse: ', () {
+    test('Termination before starting', () async {
+      var ping = Ping('1.1.1.1', count: 5);
+      expect(ping.stop(), throwsException);
     });
   });
 }
