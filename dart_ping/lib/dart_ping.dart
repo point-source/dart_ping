b62239f645d1b045085bc2c55e62c15f3dfa2cd7
point-source <47544779+point-source@users.noreply.github.com>
2026-06-18 00:41:59 +0000

    feat(interface): add listNetworkInterfaces() host-interface helper (#72)

--- dart_ping/lib/dart_ping.dart
+++ dart_ping/lib/dart_ping.dart
@@ -6,3 +6,5 @@ export 'package:dart_ping/src/models/ping_response.dart';
 export 'package:dart_ping/src/models/ping_summary.dart';
 export 'package:dart_ping/src/ping_interface.dart';
 export 'package:dart_ping/src/models/ping_parser.dart';
+export 'package:dart_ping/src/interface_listing.dart' show listNetworkInterfaces;
+export 'dart:io' show NetworkInterface, InternetAddress, InternetAddressType;
